c6581cdf27a0f97dcae1426f4211faf2fa92ecb5
Oleg Shalygin <oshalygin@gmail.com>
2016-10-21 18:18:32 -0700

    Add typescript typings

--- tools/nodeVersionCheck.js
+++ tools/nodeVersionCheck.js
@@ -1,10 +1,12 @@
 /* eslint-disable */
-var exec = require('child_process').exec;
+var exec = require("child_process").exec;
 
-exec('node -v', function (err, stdout) {
-  if (err) throw err;
+exec("node -v", function (error, stdout) {
+    if (error) {
+        throw error;
+    }
 
-  if (parseFloat(stdout.slice(1)) < 4) {
-    throw new Error('React Slingshot requires node 4.0 or greater.');
-  }
+    if (parseFloat(stdout.slice(1)) < 4) {
+        throw new Error("React Slingshot requires node 4.0 or greater.");
+    }
 });
